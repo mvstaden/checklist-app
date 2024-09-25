bf36e46b5eac332c7a8e34949a3d6bed7dd8a674
Marius Van Staden <mvstaden91@gmail.com>
2024-09-26 00:07:30 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <div className="bg-slate-600 w-[450px] p-5 rounded-xl flex flex-col">
+      <h1 className="text-5xl text-white text-center  border-b-2">
+        Checkilist
+      </h1>
+      <div className="bg-lime-100 mt-4 h-[500px]">
+        <span className="bg-zinc-600 flex items-center justify-center w-6 h-6 rounded-full content-none text-2xl">
+          +
+        </span>
+      </div>
+    </div>
+  );
+}
+
+export default App;
